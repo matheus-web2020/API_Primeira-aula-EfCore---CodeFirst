a2263f2ca59dd297e0d98509dede2608abe35ba5
Matheus <matheus.fusca.lima@outlook.com>
2020-09-25 14:37:57 -0300

    0

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API_EfCore.Domains;
+using EfCore.Interfaces;
+using EfCore.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,36 +15,105 @@ namespace EfCore.Controllers
     [ApiController]
     public class ProdutosController : ControllerBase
     {
+        private readonly IProdutoRepository _produtoRepository;
+        public ProdutosController()
+        {
+            _produtoRepository = new ProdutoRepository();
+        }
+
         // GET: api/<ProdutosController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var produtos = _produtoRepository.Listar();
+                if (produtos.Count == 0)
+                    return NoContent();
+
+                return Ok(produtos);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
 
         // GET api/<ProdutosController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(Guid id)
         {
-            return "value";
+            try
+            {
+                Produto produto = _produtoRepository.BuscarPorId(id);
+                if (produto == null)
+                    return NotFound();
+
+                return Ok(produto);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST api/<ProdutosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post(Produto produto)
         {
+            try
+            {
+                _produtoRepository.Adicionar(produto);
+
+                return Ok(produto);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<ProdutosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(Guid id, Produto produto)
         {
+            try
+            {
+                var produtoTemp = _produtoRepository.BuscarPorId(id);
+
+                if (produtoTemp == null)
+                    return NotFound();
+
+                produto.Id = id;
+                _produtoRepository.Editar(produto);
+
+                return Ok(produto);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<ProdutosController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(Guid id)
         {
+            try
+            {
+                _produtoRepository.Remover(id);
+                return Ok(id);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
+
--- Interfaces/IProdutoRepository.cs
+++ Interfaces/IProdutoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using API_EfCore.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace EfCore.Interfaces
 {
     interface IProdutoRepository
     {
-    }
-}
+        List<Produto> Listar();
+        Produto BuscarPorId(Guid id);
+
+        List<Produto> BuscarPorNome(string nome);
+        void Adicionar(Produto produto);
+        void Editar(Produto produto);
+        void Remover(Guid id);
