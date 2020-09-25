eb853190a850f645c43c6913e8d602e185e85f5a
Matheus <matheus.fusca.lima@outlook.com>
2020-09-25 09:39:14 -0300

    Aula 1 pronta

---
+++ Domains/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_EfCore.Domains
+{
+    public class Pedido
+    {
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Pedido()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ Domains/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_EfCore.Domains
+{
+    public class Produto
+    {
+        [Key]
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto()
+        {
+            Id = Guid.NewGuid();
+        }
+
+    }
+}
